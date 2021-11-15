e51cb60dbee054ee39dfaa3e9987360738c9aa6f
TSA2003 <sport4fun4me@gmail.com>
2021-11-15 17:19:23 +0200

    HomeController

---
+++ MgAPI/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MgAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]       
+        public string Index()
+        {
+            return "OK";
+        }
+    }
+}
