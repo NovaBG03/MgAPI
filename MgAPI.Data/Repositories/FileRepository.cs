c616aec22c0ee21e925078fb5ed1b774ac29527d
Gergana <gerganakoleva03@schoolmath.eu>
2022-03-07 12:15:52 +0200

    #5 Task 2: FileRepository

---
+++ MgAPI.Data/Repositories/FileRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MgAPI.Data.Entities;
+
+namespace MgAPI.Data.Repositories
+{
+    public class FileRepository : BaseRepository<WebFile>
+    {
+        public FileRepository(Context context)
+            : base(context)
+        {
+
+        }
+    }
+}
