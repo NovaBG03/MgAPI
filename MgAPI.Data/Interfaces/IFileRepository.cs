e85a42fbaf8d91d248d6f9a0ba6978a800bd275e
Gergana <gerganakoleva03@schoolmath.eu>
2022-03-07 12:12:34 +0200

    Add interface for FileRepository

---
+++ MgAPI.Data/Interfaces/IFileRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MgAPI.Data.Entities;
+
+namespace MgAPI.Data.Interfaces
+{
+    public interface IFileRepository : IBaseRepository<WebFile>
+    {
+
+    }
+}
